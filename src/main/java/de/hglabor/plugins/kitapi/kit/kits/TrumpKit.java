5bc2dfb08c3ecbbf6277a2e87b5ccdec882b448b
alvaro <varotom4@gmail.com>
2021-05-19 19:53:59 +0200

    finished Trump Kit
    * alignment of the wall in y direction
    * particles on break and if the wall cant be placed
    * fixed bugs

--- src/main/java/de/hglabor/plugins/kitapi/kit/kits/TrumpKit.java
+++ src/main/java/de/hglabor/plugins/kitapi/kit/kits/TrumpKit.java
@@ -20,17 +20,12 @@ public class TrumpKit extends AbstractKit
 	public static final TrumpKit INSTANCE = new TrumpKit();
 
 	@IntArg
-	private final int with, height,distance,time;
-
-	@MaterialArg
-	private final Material wallBlock;
+	private final int with, height, distance, time;
 
 	@FloatArg
 	private final float cooldown;
 
-	private final String trumpSnowballKey;
-
-	public TrumpKit ( )
+	public TrumpKit ()
 	{
 		super( "Trump", Material.BRICKS );
 		cooldown = 45;
@@ -38,8 +33,6 @@ public class TrumpKit extends AbstractKit
 		height = 2;
 		distance = 3;
 		time = 5;
-		wallBlock = Material.BRICKS;
-		trumpSnowballKey = this.getName() + "trumpSnowBall";
 		setMainKitItem( Material.BRICKS );
 	}
 
@@ -49,57 +42,123 @@ public class TrumpKit extends AbstractKit
 	{
 		super.onPlayerRightClickKitItem( event );
 		Player player = event.getPlayer();
-		player.sendMessage( "hey" );
 		createWall( player );
 	}
 
-	private void createWall(Player player){
+	private void createWall ( Player player )
+	{
 		World world = player.getWorld();
 		Location origin = player.getEyeLocation();
 		origin.setPitch( 0 );
 		origin.add( 0, -1, 0 );
-		Vector direction = origin.getDirection();
-		origin.add( direction.multiply( distance ) );
-		Location centerLocation = origin.clone().add( direction );
-		Location rotated =origin.clone();
+
+		Location startBlock = origin.clone();
+
+		List<Block> tempLocations = new ArrayList<>();
+		List<Location> firstLayer = getBaseLine( with, origin.clone() );
+
+		boolean isOnBlock = false;
+		boolean isNotInBlock = false;
+		for ( int i = 0; i < 3; i++ )
+		{
+
+			for ( Location location : firstLayer )
+			{
+
+				if ( !Material.AIR.equals( world.getBlockAt( location.clone().add( 0, -1, 0 ) ).getType() ) )
+				{
+					isOnBlock = true;
+					break;
+				}
+			}
+			if ( !isOnBlock )
+			{
+				startBlock.setY( (int)startBlock.getY() );
+				firstLayer = getBaseLine( with, startBlock.clone() );
+			}
+			else
+			{
+				break;
+			}
+		}
+
+		for ( int i = 0; i < 4; i++ )
+		{
+			for ( Location location : firstLayer ){
+				if ( Material.AIR.equals( world.getBlockAt( location ).getType() ) ){
+					isNotInBlock = true;
+					break;
+				}
+
+			}
+			if ( !isNotInBlock ){
+				startBlock.setY( ((int) startBlock.getY()) + 1 );
+				firstLayer = getBaseLine( with, startBlock.clone() );
+			}
+			else {
+				break;
+			}
+		}
+
+		if ( isOnBlock && isNotInBlock )
+		{
+			for ( int i = 0; i < height; i++ )
+			{
+				for ( Location location : firstLayer )
+				{
+					location.setY( (int) location.getY() + i );
+					tempLocations.add( location.getBlock() );
+				}
+			}
+			for ( Block block : tempLocations )
+			{
+				if ( block.getType().equals( Material.AIR ) )
+					block.setType( Material.BRICKS );
+			}
+
+			Bukkit.getScheduler().runTaskLater( KitApi.getInstance().getPlugin(), () -> {
+				for ( Block block : tempLocations )
+				{
+					if ( block.getType().equals( Material.BRICKS ) )
+					{
+						block.setType( Material.AIR );
+						world.spawnParticle( Particle.ASH, block.getLocation().clone().add( 0, 1, 0 ), 20, 0.3, 0.5,
+								0.3, 3 );
+					}
+				}
+			}, 20 * time );
+		}
+		else
+		{
+			world.spawnParticle( Particle.ASH, player.getEyeLocation().clone().add( 0, 1, 0 ), 20, 0.3, 0.3, 0.3, 3 );
+		}
+	}
+
+	private List<Location> getBaseLine ( int with, Location baseLocation )
+	{
+		List<Location> firstLayer = new ArrayList<>();
+
+		Vector direction = baseLocation.clone().getDirection();
+		baseLocation.add( direction.clone().normalize().multiply( distance ) );
+		Location centerLocation = baseLocation.clone();
+		Location rotated = baseLocation.clone();
 		rotated.setPitch( 0 );
-		rotated.setYaw( origin.getYaw() - 90 );
+		rotated.setYaw( baseLocation.getYaw() - 90 );
 		Vector rotation = rotated.getDirection();
 		rotation.normalize();
-		Location blockLocation = centerLocation.clone().add( rotation ).subtract( 0, height/ 2, 0 );
+		Location blockLocation = centerLocation.clone().add( rotation ).subtract( 0, height / 2, 0 );
 		rotation.multiply( -1 );
-		List<Block> tempLocations = new ArrayList<>();
-		List<Location> firstLayer = new ArrayList<>();
 
 		for ( int j = 0; j < with; j++ )
 		{
-			firstLayer.add( blockLocation.clone() );
-			blockLocation.add(rotation);
+			Location location = blockLocation.clone();
+			location.setX( (int) location.getX() );
+			location.setY( (int) location.getY() );
+			location.setZ( (int) location.getZ() );
+			firstLayer.add( location );
+			blockLocation.add( rotation );
 		}
-
-		for ( int i = 0; i < height; i++ )
-		{
-			for (Location location: firstLayer )
-			{
-				location.setY( location.getY() +1 );
-				tempLocations.add( location.getBlock() );
-			}
-		}
-		for (Block block : tempLocations){
-			if ( block.getType().equals( Material.AIR ) )
-				block.setType( Material.BRICKS );
-		}
-
-		Bukkit.getScheduler().runTaskLater( KitApi.getInstance().getPlugin(), () -> {
-			player.sendMessage( "scheduler: "+ tempLocations.size() );
-			for ( Block block : tempLocations ){
-				if ( block.getType().equals( Material.BRICKS ) ){
-					block.setType( Material.AIR );
-					world.spawnParticle( Particle.ASH, block.getLocation().clone().add( 0, 1, 0 ), 20, 0.3, 0.5, 0.3, 3);
-				}
-			}
-		}, 20*time );
-
+		return firstLayer;
 	}
 
 	@Override
